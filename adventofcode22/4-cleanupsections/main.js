573783ba0e6a68d89c76cdafe3b4f4d66b9d7ad4
Hannah C. McFarlane <hannah.mcfarlane@gmail.com>
2022-12-04 15:06:50 +0000

    part one solve for test input data

---
+++ adventofcode22/4-cleanupsections/main.js
@@ -0,0 +1,54 @@
+// 2-4,6-8
+// 2-3,4-5
+// 5-7,7-9
+// 2-8,3-7
+// 6-6,4-6
+// 2-6,4-8
+
+// convert data to an array of form
+// [[a, b, c, d], [e, f, g, h]...]
+// as Numbers not strings
+// Find whether b - a is larger than d - c
+// If b - a is bigger (i.e. more sections)
+//    then check if b > d && a < c
+// If d - c is bigger
+//   then check if d > b && c < a
+// if so then the smaller set is FULLY CONTAINED
+// add to a count
+
+import fs from "node:fs";
+
+const input = fs.readFileSync("input.txt", "utf8", (err, data) => {
+    if (err) throw err;
+  });
+function formatInput(string) {
+    const arr = string.split("\r\n");
+
+    for (let i = 0; i < arr.length; i++) {
+       arr[i] = arr[i].replaceAll(",", " ").replaceAll("-", " ").split(" ").map(j => Number(j));
+    }
+  
+    return arr;
+}
+let list = formatInput(input);
+// console.log("list", list)
+
+
+function findFullyContained (set) {
+    // console.log(set)
+    // console.log("b - a", set[1] - set[0])
+    // console.log("d - c",set[3] - set[2])
+    let returnVal = true;
+    if (set[1] - set[0] >= set[3] - set[2]) {
+        set[1] >= set[3] && set[0] <= set[2] ? returnVal : returnVal = false;
+    } else {
+        set[3] >= set[1] && set[2] <= set[0] ? returnVal : returnVal = false;
+    }
+    return returnVal;
+}
+
+let count = 0;
+let shortList = list.slice(0,5);
+shortList.map(l => findFullyContained(l) ? count++ : count)
+
+console.log("count (number of overlapping pairs):", count)
