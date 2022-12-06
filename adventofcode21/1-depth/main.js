576467ffd2b010a3cd865736ffdb367e47a9e7a1
Hannah C. McFarlane <hannah@hannahmcfarlane.co.uk>
2022-12-06 19:51:24 +0000

    y21 day1: solve for part one

---
+++ adventofcode21/1-depth/main.js
@@ -0,0 +1,43 @@
+import fs from "node:fs";
+
+const input = fs.readFileSync("inputtest.txt", "utf8", (err, data) => {
+  if (err) throw err;
+});
+
+function formatInput(string) {
+  let arr = string.split("\r\n");
+  let arrNumbers = arr.map((x) => Number(x));
+  return arrNumbers;
+}
+
+let readings = formatInput(input);
+console.dir(readings, { maxArrayLength: null });
+console.log(readings.length);
+
+let count = 0;
+
+function findIncrease(reading1, reading2) {
+  if (reading2 > reading1) {
+    count++;
+  }
+  return;
+}
+
+function countIncreases(readings) {
+  let window = 1;
+  for (let i = window * 2 - 1; i < readings.length; i++) {
+    //for part 1:
+    findIncrease(readings[i - 1], readings[i]);
+    //for part 2:
+    // findIncrease(
+    //   readings[i - 5] + readings[i - 4] + readings[i - 3],
+    //   readings[i - 2] + readings[i - 1] + readings[i]
+    // );
+    //for arbitrary sliding window
+    // let sliding1 = 0;
+    // let sliding2 = 0;
+  }
+  return count;
+}
+
+console.log("number of increases", countIncreases(readings));
