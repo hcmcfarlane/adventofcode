1343544452cf026a6ae266089ece57a1e8b747e6
Hannah C. McFarlane <hannah@hannahmcfarlane.co.uk>
2022-12-06 10:59:39 +0000

    day 6: solve for test input 1

--- adventofcode22/6-markers/main.js
+++ adventofcode22/6-markers/main.js
@@ -11,23 +11,42 @@ function formatTestInput(input) {
   return arr;
 }
 let streams = formatTestInput(testInput);
-console.log("streams", streams);
+// console.log("streams", streams);
 
 function findMarker(datastream) {
   let marker = 0;
   for (let i = 3; i < datastream.length; i++) {
-    if (
-      datastream[i] === datastream[i - 1] ||
-      datastream[i] === datastream[i - 2] ||
-      datastream[i] === datastream[i - 3]
-    ) {
-      break;
+    console.log(i, [
+      datastream[i - 3],
+      datastream[i - 2],
+      datastream[i - 1],
+      datastream[i],
+    ]);
+
+    if (datastream[i] === datastream[i - 1]) {
+      i += 2;
+    } else if (datastream[i] === datastream[i - 2]) {
+      i += 1;
+    } else if (datastream[i] === datastream[i - 3]) {
     } else {
-      marker = i;
+      marker = i + 1;
+      break;
     }
+
+    // if (
+    //   datastream[i] === datastream[i - 1] ||
+    //   datastream[i] === datastream[i - 2] ||
+    //   datastream[i] === datastream[i - 3]
+    // ) {
+    //   marker = marker;
+    // } else {
+    //   marker = i + 1;
+    //   break;
+    // }
   }
   return marker;
 }
 
 let marker = findMarker(streams[0]);
 console.log("marker", marker);
+console.log(streams[0].length);
