f0a5040a258b229d1e19d45647db112166e39681
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-12-09 13:43:03 +0000

    22 d9: solve for part one

--- adventofcode22/9-rope/main.js
+++ adventofcode22/9-rope/main.js
@@ -10,16 +10,36 @@ function formatTestInput(input) {
 }
 
 let moves = formatTestInput(input);
-console.log("moves", moves);
+// console.log("moves", moves);
 
-function trackPositionOfHead(movements) {
+function trackPositionOfHead(movements, numOfKnots) {
 	let [xH, yH] = [0, 0];
-	let [xT, yT] = [0, 0];
+
+	//set up an arbitrary number of knots with starting coords [0,0]
+	let coords = {};
+	for (let k = 0; k <= numOfKnots; k++) {
+		let nameX = "x" + k;
+		let nameY = "y" + k;
+		// console.log(nameX, nameY);
+		// [eval(nameX), eval(nameY)] = [0, 0];
+		coords[nameX] = 0;
+		coords[nameY] = 0;
+	}
+	// console.log([coords.x9, coords.y0]);
+	// let [x1, y1] = [0, 0];
+	// let [x2, y2] = [0, 0];
+	// let [x3, y3] = [0, 0];
+	// let [x4, y4] = [0, 0];
+	// let [x5, y5] = [0, 0];
+	// let [x6, y6] = [0, 0];
+	// let [x7, y7] = [0, 0];
+	// let [x8, y8] = [0, 0];
+	// let [x9, y9] = [0, 0];
 	// let [maxL, maxR, maxU, maxD] = [0, 0, 0, 0];
 	// let maxDim; // largest size of array
 
 	for (let i = 0; i < movements.length; i++) {
-		console.log(...movements[i]);
+		// console.log(...movements[i]);
 		let direction = movements[i][0];
 		let steps = Number(movements[i][1]);
 
@@ -45,43 +65,66 @@ function trackPositionOfHead(movements) {
 					throw Error("Unexpected directional input");
 					break;
 			}
-			[xT, yT] = trackPositionOfTail(xH, yH, xT, yT);
+			//
+			[xT, yT] = trackPositionOfKnot(xH, yH, xT, yT, false);
 		}
 
 		// Math.abs(y) > max ? (max = y) : "";
 		// maxDim = Math.max(maxR - maxL, maxU - maxD);
 		// console.log([xH, yH], "\n");
-		console.log("\n");
+		// console.log("\n");
 	}
 
 	// createBlankArray(maxDim);
 	return [xH, yH];
 }
 
-function trackPositionOfTail(xh, yh, xt, yt) {
+function trackPositionOfKnot(xh, yh, xt, yt, lastKnot) {
 	//check if already adjacent
 	if (
 		(xh - 1 === xt || xh + 1 === xt || xh === xt) &&
 		(yh - 1 === yt || yh + 1 === yt || yh === yt)
 	) {
+		// console.log([xh, yh], [xt, yt]);
 		return [xt, yt];
 	}
-	//if vertically aligned
-	//follows it up or down wards
+	//if vertically or horiz aligned
+	//follows it left or right, or up/down
+	//if not vert or horiz aligned, move diagonally
 	if (yh === yt) {
-		if (xh > xt) {
-			xt -= 1;
-		} else {
-			xt += 1;
-		}
+		xh > xt ? (xt += 1) : (xt -= 1);
+	} else if (xh === xt) {
+		yh > yt ? (yt += 1) : (yt -= 1);
 	} else {
-		xh > xt ? (xt -= 1) : (xt += 1);
+		xh > xt ? (xt += 1) : (xt -= 1);
+		yh > yt ? (yt += 1) : (yt -= 1);
 	}
-
-	console.log([xh, yh], [xt, yt]);
+	if (lastKnot && !allPositionsOfTail.includes([xt, yt])) {
+		allPositionsOfTail.push([xt, yt]);
+	}
+	// console.log([xh, yh], [xt, yt]);
 	return [xt, yt];
 }
 
-// function createBlankArray(size) {}
+function findUniqueTailPositions(pos) {
+	function uniqBy(a, key) {
+		var seen = {};
+		return a.filter(function (item) {
+			var k = key(item);
+			return seen.hasOwnProperty(k) ? false : (seen[k] = true);
+		});
+	}
+	let unique = uniqBy(pos, JSON.stringify);
+	console.log("count of unique", unique.length);
+	// console.log("unique", unique);
+	return unique;
+}
 
-trackPositionOfHead(moves);
+// function createBlankArray(size) {}
+let allPositionsOfTail = [[0, 0]];
+const numberOfKnots = 9;
+trackPositionOfHead(moves, numberOfKnots);
+findUniqueTailPositions(allPositionsOfTail);
+
+// console.log("number of tail positions:", allPositionsOfTail.length);
+// console.log(allPositionsOfTail);
