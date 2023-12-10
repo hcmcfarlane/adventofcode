546a3584c422d23be7091dadce05e620855f6043
Hannah McFarlane <hannah@hannahmcfarlane.co.uk>
2023-12-10 12:50:03 +0000

    23 d2: update input formatter

--- adventofcode23/src/utils/formatSingleLineInput/formatSingleLineInput.test.ts
+++ adventofcode23/src/utils/formatSingleLineInput/formatSingleLineInput.test.ts
@@ -1,8 +1,13 @@
 import formatSingleLineInput from "./formatSingleLineInput";
 
 describe("formatInput", () => {
-  it("should convert input to an array", () => {
+  it("should convert input with line breaks to an array", () => {
     const result = formatSingleLineInput("1\n2\n3");
+    expect(result).toEqual(["1", "2", "3"]);
+  });
+
+  it("should convert input with returns and line breaks to an array", () => {
+    const result = formatSingleLineInput("1\r\n2\r\n3", true);
     expect(result).toEqual(["1", "2", "3"]);
   });
 });
--- adventofcode23/src/utils/formatSingleLineInput/formatSingleLineInput.ts
+++ adventofcode23/src/utils/formatSingleLineInput/formatSingleLineInput.ts
@@ -1,5 +1,9 @@
-const formatSingleLineInput = (input: string) => {
-  return input.trim().split("\n");
+const formatSingleLineInput = (input: string, isRN?: boolean | undefined) => {
+  if (isRN) {
+    return input.trim().split("\r\n");
+  } else {
+    return input.trim().split("\n");
+  }
 };
 
 export default formatSingleLineInput;
